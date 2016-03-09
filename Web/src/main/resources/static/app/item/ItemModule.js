51bf3a41bd5add0719ba545fec2f367d54883b54
Braidner <goodly92@gmail.com>
2016-03-09 22:27:59 +0300

    update new styles

--- Web/src/main/resources/static/app/item/ItemModule.js
+++ Web/src/main/resources/static/app/item/ItemModule.js
@@ -1,3 +1,13 @@
 /**
  * Created by goodl on 3/9/2016.
  */
+(function () {
+    angular.module('ItemModule', [])
+        .directive('contentItem', contentItem);
+
+    function contentItem() {
+        return {
+            restrict: 'E'
+        }
+    }
+})();
