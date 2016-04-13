e1d370b574aa785a0c9e62a9058eaf9d5a5f757e
Braidner <work>
2016-04-13 15:59:16 +0300

    update static

--- Web/src/main/resources/static/dist/all.js
+++ Web/src/main/resources/static/dist/all.js
@@ -203,6 +203,18 @@
 })();
 
 /**
+ * Created by goodl on 4/2/2016.
+ */
+(function() {
+    "use strict";
+
+    angular.module('ControlsModule', [
+        'control.input-control', 
+        'control.parser',
+        'control.input-chunk'
+    ]);
+})();
+/**
  * Created by goodl on 3/13/2016.
  */
 (function () {
@@ -232,18 +244,6 @@
         "ngInject";
     }
     
-})();
-/**
- * Created by goodl on 4/2/2016.
- */
-(function() {
-    "use strict";
-
-    angular.module('ControlsModule', [
-        'control.input-control', 
-        'control.parser',
-        'control.input-chunk'
-    ]);
 })();
 /**
  * Created by goodl on 3/9/2016.
@@ -307,6 +307,69 @@
     
 })();
 /**
+ * Created by Braidner
+ */
+(function () {
+    'use strict';
+
+    angular.module('RecentModule', [])
+        .controller("RecentCtrl", RecentCtrl);
+
+    RecentCtrl.$inject = ['$scope'];
+    function RecentCtrl($scope) {
+        $scope.content = [
+            {name: 'Zootopia', poster: 'http://www.kinopoisk.ru/images/film_big/775276.jpg', id: 0, type: 'film'},
+            {name: 'Batman v Superman: Dawn of Justice', poster: 'http://www.kinopoisk.ru/images/film_big/770631.jpg', id: 0, type: 'film'},
+            {name: 'The Justice League Part One', poster: 'http://www.kinopoisk.ru/images/film_big/424994.jpg', id: 0, type: 'film'},
+            {name: 'The flash', poster: 'http://www.kinopoisk.ru/images/film_big/817506.jpg', id: 0, type: 'film'},
+            {name: 'The Justice League Part One', poster: 'http://www.kinopoisk.ru/images/film_big/424994.jpg', id: 0, type: 'film'},
+            {name: 'Frozen', poster: 'http://www.kinopoisk.ru/images/film_big/493208.jpg', id: 0, type: 'film'},
+            {name: 'Batman v Superman: Dawn of Justice', poster: 'http://www.kinopoisk.ru/images/film_big/770631.jpg', id: 0, type: 'film'},
+            {name: 'Untitled Spider-Man Reboot', poster: 'http://www.kinopoisk.ru/images/film_big/690593.jpg', id: 0, type: 'film'}
+        ];
+    }
+
+    angular.module('RecentModule').directive('owlCarousel', function () {
+
+        var linker = function (scope, element, attr) {
+
+            var loadCarousel = function () {
+                element.owlCarousel(
+                    {
+                        loop: false,
+                        nav: false,
+                        responsive: {
+                            1200: {
+                                items: 6
+                            },
+                            992: {
+                                items: 4
+                            },
+                            768: {
+                                items: 3
+                            },
+                            479: {
+                                items: 2
+                            }
+                        }
+                    }
+                );
+            };
+
+            scope.$watchCollection(attr.owlCarousel, function () {
+                loadCarousel();
+            });
+
+        };
+
+        return {
+            restrict: "A",
+            link: linker
+        };
+
+    });
+})();
+/**
  * Created by Braidner on 06/03/2016.
  */
 (function () {
@@ -352,69 +415,6 @@
         };
     }
 
-})();
-/**
- * Created by Braidner
- */
-(function () {
-    'use strict';
-
-    angular.module('RecentModule', [])
-        .controller("RecentCtrl", RecentCtrl);
-
-    RecentCtrl.$inject = ['$scope'];
-    function RecentCtrl($scope) {
-        $scope.content = [
-            {name: 'Zootopia', poster: 'http://www.kinopoisk.ru/images/film_big/775276.jpg', id: 0},
-            {name: 'Batman v Superman: Dawn of Justice', poster: 'http://www.kinopoisk.ru/images/film_big/770631.jpg', id: 0},
-            {name: 'The Justice League Part One', poster: 'http://www.kinopoisk.ru/images/film_big/424994.jpg', id: 0},
-            {name: 'The flash', poster: 'http://www.kinopoisk.ru/images/film_big/817506.jpg', id: 0},
-            {name: 'The Justice League Part One', poster: 'http://www.kinopoisk.ru/images/film_big/424994.jpg', id: 0},
-            {name: 'Frozen', poster: 'http://www.kinopoisk.ru/images/film_big/493208.jpg', id: 0},
-            {name: 'Batman v Superman: Dawn of Justice', poster: 'http://www.kinopoisk.ru/images/film_big/770631.jpg', id: 0},
-            {name: 'Untitled Spider-Man Reboot', poster: 'http://www.kinopoisk.ru/images/film_big/690593.jpg', id: 0}
-        ];
-    }
-
-    angular.module('RecentModule').directive('owlCarousel', function () {
-
-        var linker = function (scope, element, attr) {
-
-            var loadCarousel = function () {
-                element.owlCarousel(
-                    {
-                        loop: false,
-                        nav: false,
-                        responsive: {
-                            1200: {
-                                items: 6
-                            },
-                            992: {
-                                items: 4
-                            },
-                            768: {
-                                items: 3
-                            },
-                            479: {
-                                items: 2
-                            }
-                        }
-                    }
-                );
-            };
-
-            scope.$watchCollection(attr.owlCarousel, function () {
-                loadCarousel();
-            });
-
-        };
-
-        return {
-            restrict: "A",
-            link: linker
-        };
-
-    });
 })();
 /**
  * Created by goodl on 4/2/2016.
